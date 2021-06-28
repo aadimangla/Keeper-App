ed725a0b35a503a004f7cd284dca7cc641536385
Aditya Mangla <aadimangla@gmail.com>
2021-06-28 10:21:48 +0530

    Part -1

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Note from './Note';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+    return <div>
+        <Header></Header>
+        <Note></Note>
+        <Footer></Footer>
+    </div>
+}
+
+export default App;
